3bef0b38154b651c269d33239637ac5c4a928a3d
ketan chokkara <chokkaraketanrao@gmail.com>
2026-05-13 23:34:51 +0530

    feat : Backend completed with callbacks with token and pipeline

--- ai-token-assistant/index.js
+++ ai-token-assistant/index.js
@@ -3,6 +3,11 @@ import mongoose from "mongoose"
 import cors from "cors"
 import dotenv from "dotenv"
 import userRoutes from './routes/user'
+import {serve} from "inngest/express"
+import tokenRoutes from './routes/token'
+import { inngest } from "./inngest/client"
+import {onUserSignup} from "./inngest/functions/onsignup"
+import {onTokenCreated} from "./inngest/functions/ont-token-create"
 dotenv.config()
 
 const PORT = process.env.PORT || 3000
@@ -11,6 +16,15 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/auth',userRoutes);
+app.use('/api/tokens',tokenRoutes);
+
+
+app.use("/api/inngest",
+    serve({
+        client:inngest,
+        functions:[onUserSignup,onTokenCreated]
+    })
+);
 
 
 mongoose
--- ai-token-assistant/routes/token.js
+++ ai-token-assistant/routes/token.js
@@ -1,5 +1,14 @@
 import express from 'express'
+import {authenticate} from '../inngest/middlewares/auth'
+import { getUsers, login, logout, signup, updateUser } from '../inngest/controllers/user'
+import { createToken, getToken, getTokens } from '../controllers/token'
+
 
 const router = express.Router()
+
+router.get("/",authenticate,getTokens)
+router.get("/:id",authenticate,getToken)
+router.post("/",authenticate,createToken)
+
 
 export default router;
